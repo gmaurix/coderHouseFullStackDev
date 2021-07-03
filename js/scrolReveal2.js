5cd45a8b670653e8c3dc27f0feecf7fbedd8e4d8
gmaurix <utn.mauri@gmail.com>
2021-07-03 10:56:31 -0300

    agregado de librerias js y bootstrap

---
+++ js/scrolReveal2.js
@@ -0,0 +1,37 @@
+window.sr = ScrollReveal();
+sr.reveal(".encabezado__navbar", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "-100px",
+});
+sr.reveal(".encabezado__logo", {
+  duration: 3000,
+  origin: "rigth",
+  distance: "-100px",
+});
+sr.reveal(".inicio__titulo", {
+  duration: 3000,
+  origin: "top",
+  distance: "-100px",
+});
+sr.reveal(".inicio__info", {
+  duration: 3000,
+  origin: "top",
+  distance: "-100px",
+});
+sr.reveal(".nosotros__imagen", {
+  duration: 2000,
+  origin: "rigth",
+  distance: "-100px",
+});
+
+sr.reveal(".nosotros__titulo", {
+  duration: 2000,
+  origin: "rigth",
+  distance: "-100px",
+});
+sr.reveal(".nosotros__definicion", {
+  duration: 2000,
+  origin: "top",
+  distance: "-100px",
+});
