2f2f34f6d47cdb84a69836d5f33f80674861ac3d
gmaurix <utn.mauri@gmail.com>
2021-07-22 13:16:19 -0300

    aplicando boostrap en barra de navegacion, galeria, carta

---
+++ js/galeria.js
@@ -0,0 +1,15 @@
+let imagenes=[1,2,3,4,5,6,7,8,9,10,11,12];
+let gal= document.getElementById('galeria');
+
+for(imagen of imagenes){
+    galeria.innerHTML +=`
+    <div class="card justify-content-evenly bg-transparent" style="margin:1em;">
+        <a href="#" data-bs-toggle="modal" data-bs-target="#imagen${imagen}" >
+            <img src="../img/galeria/${imagen}.jpg" alt="" class="card-img-top" >
+        </a>
+    </div>
+    <div class="modal fade" id="imagen${imagen}" tabindex="-1" role="dialog" aria-labelledby="imagen" aria-hidden="true">    <div class="modal-dialog modal-dialog-centered" role="document">
+              <img src="../img/galeria/${imagen}.jpg" class="img-fluid rounded" alt=""> 
+     </div> 
+       
+    `;}
