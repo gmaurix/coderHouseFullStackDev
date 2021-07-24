2c1a843a909c967739eb9e3cd6d85f424ec2dd85
gmaurix <utn.mauri@gmail.com>
2021-07-24 14:59:23 -0300

    completar seccion nosotros

--- js/galeria.js
+++ js/galeria.js
@@ -5,11 +5,10 @@ for(imagen of imagenes){
     galeria.innerHTML +=`
     <div class="card justify-content-evenly bg-transparent" style="margin:1em;">
         <a href="#" data-bs-toggle="modal" data-bs-target="#imagen${imagen}" >
-            <img src="../img/galeria/${imagen}.jpg" alt="" class="card-img-top" >
+            <img src="../img/galeria/${imagen}.jpg" alt="imagen galeria" class="card-img-top" loading="lazy" >
         </a>
     </div>
     <div class="modal fade" id="imagen${imagen}" tabindex="-1" role="dialog" aria-labelledby="imagen" aria-hidden="true">    <div class="modal-dialog modal-dialog-centered" role="document">
-              <img src="../img/galeria/${imagen}.jpg" class="img-fluid rounded" alt=""> 
-     </div> 
-       
+              <img src="../img/galeria/${imagen}.jpg" class="img-fluid rounded" alt="imagen galeria" loading="lazy"> 
+     </div>        
     `;}
