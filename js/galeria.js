2728d0a4a33e0aa7610e8728a68fcda9638fe31c
mauri <utn.mauri@gmail.com>
2021-08-02 19:53:01 -0300

    cambios para subir al servidor

--- js/galeria.js
+++ js/galeria.js
@@ -1,14 +1,15 @@
-let imagenes=[1,2,3,4,5,6,7,8,9,10,11,12];
-let gal= document.getElementById('galeria');
+let imagenes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let gal = document.getElementById("galeria");
 
-for(imagen of imagenes){
-    galeria.innerHTML +=`
-    <div class="card justify-content-evenly bg-transparent" style="margin:1em;">
+for (imagen of imagenes) {
+  galeria.innerHTML += `
+    <div class="card justify-content-evenly bg-transparent" style="margin:0.2em; width:15em; ">
         <a href="#" data-bs-toggle="modal" data-bs-target="#imagen${imagen}" >
-            <img src="../img/galeria/${imagen}.jpg" alt="imagen galeria" class="card-img-top" loading="lazy" >
+            <img src="../img/galeria/${imagen}.jpg" width="auto" height="auto" alt="imagen galeria" class=" img-fluid card-img-top" loading="lazy" >
         </a>
     </div>
     <div class="modal fade justify-content-center" id="imagen${imagen}" tabindex="-1" role="dialog" aria-labelledby="imagen" aria-hidden="true">    <div class="modal-dialog modal-dialog-centered" role="document">
-              <img src="../img/galeria/${imagen}.jpg" class="img-fluid rounded" alt="imagen galeria" loading="lazy"> 
+              <img src="../img/galeria/${imagen}.jpg" class="img-fluid rounded" width="auto" height="auto" alt="imagen galeria" loading="lazy"> 
      </div>        
-    `;}
+    `;
+}
